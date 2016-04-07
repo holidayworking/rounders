eae0f6541cf0a4d61ab775230e1b60d3bf6c158d
akira.takahashi <rike422@gmail.com>
2016-04-07 19:08:16 +0900

    [from now] 2016/04/07 19:08:16

--- lib/rounders/receivers/mail.rb
+++ lib/rounders/receivers/mail.rb
@@ -2,6 +2,42 @@ module Rounders
   module Receivers
     class Mail < Receiver
 
+      DEFALUT_FIND_OPTION = {
+        keys: %w(NOT SEEN)
+      }.freeze
+      attr_reader :client, :find_options
+
+      def initialize(client: nil, find_options: {})
+        @client = client
+        @find_options = DEFALUT_FIND_OPTION.merge(find_options)
+      end
+
+      def configure
+        config = Config.new
+        yield config
+        @client = Receiver.create_client(config)
+      end
+
+      def receive
+        @client.
+          find(find_options).
+          map { |message| Rounders::Mail.new(message) }
+      end
+
+      class << self
+        def configure
+          config = Config.new
+          yield config
+          @receiver = create_client(config)
+        end
+
+        def create_client(config)
+          retriever = parser.lookup_retriever_method(config.protocol)
+          new(
+            client: retriever.new(config.mail_server_settings),
+            find_options: config.find_options)
+        end
+      end
     end
   end
 end
